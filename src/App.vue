0f75a6b17aa6c067b58a5ef35619b5f5f5cac671
ed <edgardo.medina@ironman.com>
2020-12-27 14:14:00 -0500

    removed top nav bar

--- src/App.vue
+++ src/App.vue
@@ -2,43 +2,7 @@
   <v-app>
     
 
-    <v-app-bar
-      color="#d2202f"
-      dark
-      prominent
-    >
-      <v-app-bar-nav-icon @click.stop="drawer = !drawer"></v-app-bar-nav-icon>
-
-      <v-toolbar-title>Digital</v-toolbar-title>
-
-      <v-spacer></v-spacer>
-    </v-app-bar>
-
-    <v-navigation-drawer
-      v-model="drawer"
-      absolute
-      bottom
-      temporary
-    >
-      <v-list
-        nav
-        dense
-      >
-        <v-list-item-group
-          v-model="group"
-          active-class="deep-purple--text text--accent-4"
-        >
-          <v-list-item>
-            <v-list-item-title >Logout</v-list-item-title>
-          </v-list-item>
-
-          <v-list-item>
-            <v-list-item-title>Account</v-list-item-title>
-          </v-list-item>
-
-        </v-list-item-group>
-      </v-list>
-    </v-navigation-drawer>
+    
 
     <v-content>
        <router-view></router-view>
